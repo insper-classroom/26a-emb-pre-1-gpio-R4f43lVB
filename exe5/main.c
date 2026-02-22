ad302a02b877fa656eb19b9a5898b0f42ad39a54
Rafael Vieira Bravo <rafaelvb@al.insper.edu.br>
2026-02-21 23:17:36 -0300

    Mudei o atraso no 5 de 150 pra 50

--- exe5/main.c
+++ exe5/main.c
@@ -21,16 +21,14 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
-            sleep_ms(150);
-            cnt_1++;
-            printf("Botao 1: %d\n", cnt_1);
+            sleep_ms(50);
+            printf("Botao 1: %d\n", cnt_1++);
             while (!gpio_get(BTN_PIN)) {};
         }
 
         if (!gpio_get(BTN_PIN_2)) {
-            sleep_ms(150);
-            cnt_2++;
-            printf("Botao 2: %d\n", cnt_2);
+            sleep_ms(50);
+            printf("Botao 2: %d\n", cnt_2++);
             while (!gpio_get(BTN_PIN_2)) {};
         }
     }
