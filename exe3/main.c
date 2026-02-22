a8bb1e3a92c40298fbd567372307fb006f8f7d18
Rafael Vieira Bravo <rafaelvb@al.insper.edu.br>
2026-02-21 23:04:23 -0300

    Todo o resto

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,43 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_VERM = 4;
+const int BOTAO_VERM = 28;
+const int LED_VERDE = 6;
+const int BOTAO_VERDE = 26;
+
+
 int main() {
   stdio_init_all();
+  int verde = 0;
+  int vermelho = 0;
+
+  // Configuracao dos pinos
+  gpio_init(BOTAO_VERM);
+  gpio_set_dir(BOTAO_VERM, GPIO_IN);
+  gpio_pull_up(BOTAO_VERM);
+
+  gpio_init(BOTAO_VERDE);
+  gpio_set_dir(BOTAO_VERDE, GPIO_IN);
+  gpio_pull_up(BOTAO_VERDE);
+  
+  gpio_init(LED_VERDE);
+  gpio_set_dir(LED_VERDE, GPIO_OUT);
+
+  gpio_init(LED_VERM);
+  gpio_set_dir(LED_VERM, GPIO_OUT);
 
   while (true) {
+    if (!gpio_get(BOTAO_VERDE)) {
+      verde = !verde;
+      while (!gpio_get(BOTAO_VERDE)) {};
+      gpio_put(LED_VERDE, verde);
+    }
+    if (!gpio_get(BOTAO_VERM)) {
+      vermelho = !vermelho;
+      while (!gpio_get(BOTAO_VERM)) {};
+      gpio_put(LED_VERM, vermelho);
+    }
+
   }
 }
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,42 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_VERM = 5;
+const int LED_ROXO = 8;
+const int LED_AZUL = 11;
+const int LED_AMAR = 15;
+const int BOTAO = 28;
+
 int main() {
   stdio_init_all();
+  
+  gpio_init(BOTAO);
+  gpio_set_dir(BOTAO, GPIO_IN);
+  gpio_pull_up(BOTAO);
+
+  gpio_init(LED_AMAR);
+  gpio_set_dir(LED_AMAR, GPIO_OUT);
+
+  gpio_init(LED_AZUL);
+  gpio_set_dir(LED_AZUL, GPIO_OUT);
+
+  gpio_init(LED_ROXO);
+  gpio_set_dir(LED_ROXO, GPIO_OUT);
+
+  gpio_init(LED_VERM);
+  gpio_set_dir(LED_VERM, GPIO_OUT);
+
+  int leds[4] = {LED_VERM, LED_ROXO, LED_AZUL, LED_AMAR};
 
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if (!gpio_get(BOTAO)) {
+      for (int i=0;i<4;i++) {
+        gpio_put(leds[i], 1);
+        sleep_ms(300);
+        gpio_put(leds[i], 0);
+        sleep_ms(300);
+      }
+      while (!gpio_get(BOTAO)) {};
+    }
   }
 }
